b46fbe3ac21701edb1d1ade72d62c5f2cb138bea
skippershark <sharmanfaridz@gmail.com>
2025-08-20 11:09:18 +0800

    end of day progress

--- index.js
+++ index.js
@@ -48,45 +48,6 @@ const myLibrary = new Library(
     ]
 )
 
-// function updateDisplayedBooks() {
-//     const shownBookList = document.getElementById("bookList")
-//     const shownBooks = shownBookList.getElementsByTagName("li")
-//     const shownBookIds = Array.from(shownBooks).map(li => li.getAttribute("id"))
-
-//     // ensure books that should not be shown are deleted
-//     for (let shownBookId of shownBookIds) {
-//         if (!myLibrary.hasBook(shownBookId)) {
-//             document.getElementById(shownBookId).remove()
-//         }
-//     }
-
-//     //ensure books that should be shown are shown
-//     myLibrary.books.forEach( (book) => {
-//         if (shownBookIds.includes(book.id)) return
-
-//         const newLi = document.createElement("li")
-//         newLi.innerText = book.name
-//         newLi.setAttribute("id", book.id)
-
-//         const readButton = document.createElement("button")
-//         readButton.innerText = "Read"
-//         readButton.addEventListener("click" (e) => {
-//             e.preventDefault()
-//             myLibrary
-//         })
-
-//         const undoButton = document.createElement("button")
-//         undoButton.innerText = "Undo"
-//         undoButton.addEventListener("click", (e) => {
-//             e.preventDefault()
-//             myLibrary.removeBook(book.id)
-//             updateDisplayedBooks()
-//         })
-        
-//         newLi.appendChild(undoButton)
-//         shownBookList.appendChild(newLi)
-//     })
-// }
 function updateShownBookList() {
     const shownBookList = document.getElementById("bookList")
 
@@ -139,20 +100,6 @@ newBookButton.addEventListener("click", (event) => {
 })
 
 
-// submitNewBookButton.addEventListener("click", (event) => {
-//     event.preventDefault()
-
-//     const form = document.getElementById("newBookForm")
-//     const formData = new FormData(form)
-//     const bookName = formData.get("bookName")
-//     let newBookAdded = myLibrary.addNewBook(bookName)
-//     if (!newBookAdded) {
-//         alert("This book was already added!")
-//     }
-//     updateShownBookList()
-//     // form.reset()
-//     dialog.close()
-// })
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(form)
